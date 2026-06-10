2658a056023af334975e3d019b1ff8413585eaac
vitor244hugo <vitor.cauvilla.almeida@escola.pr.gov.br>
2026-06-10 14:40:42 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,30 @@
+function atualizarSensores() {
 
+    let temperatura = (Math.random() * 15 + 20).toFixed(1);
+    let umidade = Math.floor(Math.random() * 40 + 40);
+
+    let qualidadeAr = [
+        "Excelente ✅",
+        "Boa 👍",
+        "Moderada ⚠️",
+        "Ruim ❌"
+    ];
+
+    let ar =
+        qualidadeAr[Math.floor(Math.random() * qualidadeAr.length)];
+
+    document.getElementById("temperatura").textContent =
+        temperatura + " °C";
+
+    document.getElementById("umidade").textContent =
+        umidade + " %";
+
+    document.getElementById("ar").textContent =
+        ar;
+}
+
+// Atualização automática ao abrir a página
+atualizarSensores();
+
+// Atualiza a cada 5 segundos
+setInterval(atualizarSensores, 5000);
